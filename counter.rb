d22e49ee39233c9abcc96b7e53888c040cdeb727
Darren Burgess <darrentburgess@gmail.com>
2016-06-23 10:49:20 -0400

    add back parse request method

--- counter.rb
+++ counter.rb
@@ -3,12 +3,24 @@ require "pry"
 
 server = TCPServer.new "localhost", 3003
 
+def parse_request(request)
+  http_method, path_and_params, http = request.split(' ')
+  path, params = path_and_params.split('?')
+
+  params = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split('=')
+    hash[key] = value
+  end unless params == nil
+
+  [http_method, path, params]
+end
+
 loop do
   client = server.accept
   request_line = client.gets
   puts request_line
 
-  http_method, path, params = parse_params(request_line) 
+  http_method, path, params = parse_request(request_line) 
 
   client.puts "HTTP/1.0 200 OK"
   client.puts "Content-Type: text/html"
@@ -23,7 +35,10 @@ loop do
   client.puts params
   client.puts "</pre>"
 
+  client.puts "<h1>Counter</h1>"
 
+  number = params["number"].to_i
+  client.puts "<p> The current number is #{number}.</p>"
 
   client.puts "</body>"
   client.puts "</html>"
