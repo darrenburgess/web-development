d8be18fc2b3c37927f5c3d58be2e95591db66767
Darren Burgess <darrentburgess@gmail.com>
2016-06-23 10:00:53 -0400

    add nil check for params

--- simple_echo_server.rb
+++ simple_echo_server.rb
@@ -4,10 +4,17 @@ require "pry"
 server = TCPServer.new "localhost", 3003
 
 def parse_params(request)
-  params = /\?.+ /.match(request).to_s.chop
-  params.slice! 0
-  array = params.split("&")
-  Hash[ array.map! { |param| param.split("=") } ]
+  http_method, path_and_params, http = request.split(' ')
+  path, params = path_and_params.split('?')
+
+  if params
+    params = params.split("&").each_with_object({}) do |pair, hash|
+      key, value = pair.split('=')
+      hash[key] = value
+    end
+  end
+
+  [http_method, path, params]
 end
 
 loop do
@@ -15,21 +22,21 @@ loop do
   request_line = client.gets
   puts request_line
 
-  http_method = request_line.split.first
-  path = /\/.+\?/.match(request_line).to_s.chop
-  params = parse_params(request_line)
+  http_method, path, params = parse_params(request_line) 
 
   client.puts request_line
   client.puts http_method
   client.puts path
   client.puts params
 
-  rolls = params["rolls"].to_i
-  sides = params["sides"].to_i
+  if params
+    rolls = params["rolls"].to_i
+    sides = params["sides"].to_i
 
-  rolls.to_i.times do |roll|
-    roll += 1
-    client.puts "roll #{roll}: #{rand(sides.to_i) + 1} on #{sides} sided die"
+    rolls.to_i.times do |roll|
+      roll += 1
+      client.puts "roll #{roll}: #{rand(sides.to_i) + 1} on #{sides} sided die"
+    end
   end
 
   client.close
