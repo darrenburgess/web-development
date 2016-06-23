ed06d77cdd9b14e1923bebeeaba430cd4e76b77e
Darren Burgess <darrentburgess@gmail.com>
2016-06-23 10:42:12 -0400

    remove dice rolling code

--- counter.rb
+++ counter.rb
@@ -3,24 +3,10 @@ require "pry"
 
 server = TCPServer.new "localhost", 3003
 
-def parse_params(request)
-  http_method, path_and_params, http = request.split(' ')
-  path, params = path_and_params.split('?')
-
-  params = params.split("&").each_with_object({}) do |pair, hash|
-    key, value = pair.split('=')
-    hash[key] = value
-  end unless params == nil
-
-  [http_method, path, params]
-end
-
 loop do
   client = server.accept
   request_line = client.gets
   puts request_line
-
-  next unless request_line
 
   http_method, path, params = parse_params(request_line) 
 
@@ -37,18 +23,7 @@ loop do
   client.puts params
   client.puts "</pre>"
 
-  client.puts "<h1>Rolls</h1>"
-  unless params == nil
-    rolls = params["rolls"].to_i
-    sides = params["sides"].to_i
 
-    rolls.to_i.times do |roll|
-      roll += 1
-      client.puts "<p>"
-      client.puts "roll #{roll}: #{rand(sides.to_i) + 1} on #{sides} sided die"
-      client.puts "</p>"
-    end
-  end
 
   client.puts "</body>"
   client.puts "</html>"
