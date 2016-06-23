798801f439425a21ebe246c6b019832f252324b6
Darren Burgess <darrentburgess@gmail.com>
2016-06-23 10:17:17 -0400

    add basic html

--- simple_echo_server.rb
+++ simple_echo_server.rb
@@ -24,20 +24,33 @@ loop do
 
   http_method, path, params = parse_params(request_line) 
 
+  client.puts "HTTP/1.0 200 OK"
+  client.puts "Content-Type: text/html"
+  client.puts
+  client.puts "<html>"
+  client.puts "<body>"
+
+  client.puts "<pre>"
   client.puts request_line
   client.puts http_method
   client.puts path
   client.puts params
+  client.puts "</pre>"
 
+  client.puts "<h1>Rolls</h1>"
   if params
     rolls = params["rolls"].to_i
     sides = params["sides"].to_i
 
     rolls.to_i.times do |roll|
       roll += 1
+      client.puts "<p>"
       client.puts "roll #{roll}: #{rand(sides.to_i) + 1} on #{sides} sided die"
+      client.puts "</p>"
     end
   end
 
+  client.puts "</body>"
+  client.puts "</html>"
   client.close
 end
