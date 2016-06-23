56e322d21ccf98f8407961a1ac606bd008dd23a9
Darren Burgess <darrentburgess@gmail.com>
2016-06-23 11:05:09 -0400

    add increment/decrement of number

--- counter.rb
+++ counter.rb
@@ -7,10 +7,10 @@ def parse_request(request)
   http_method, path_and_params, http = request.split(' ')
   path, params = path_and_params.split('?')
 
-  params = params.split("&").each_with_object({}) do |pair, hash|
+  params = (params || '').split("&").each_with_object({}) do |pair, hash|
     key, value = pair.split('=')
     hash[key] = value
-  end unless params == nil
+  end
 
   [http_method, path, params]
 end
@@ -39,6 +39,9 @@ loop do
 
   number = params["number"].to_i
   client.puts "<p> The current number is #{number}.</p>"
+
+  client.puts "<a href='?number=#{number + 1}'>Add one</a>"
+  client.puts "<a href='?number=#{number - 1}'>Subtract one</a>"
 
   client.puts "</body>"
   client.puts "</html>"
