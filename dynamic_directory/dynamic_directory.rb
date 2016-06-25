ba8f2bb8e41e18a14353b90359d01b179ee00715
Darren Burgess <darrentburgess@gmail.com>
2016-06-25 14:11:39 -0400

    route to file 1

--- dynamic_directory/dynamic_directory.rb
+++ dynamic_directory/dynamic_directory.rb
@@ -3,5 +3,13 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 get "/" do
-  "hello, world"
+  @title = "File List"
+  @files = Dir.glob('public/*.txt')
+  erb :home
+end
+
+get "/public/1" do
+  @file_name = "test1.txt"
+  @content = File.read "public/test1.txt"
+  erb :file
 end
