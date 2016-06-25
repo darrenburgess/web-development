34804adaf0b498ad44db4b49f4877fc4cbf4c265
Darren Burgess <darrentburgess@gmail.com>
2016-06-25 09:53:21 -0400

    add chapter 1

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -7,3 +7,11 @@ get "/" do
   @contents = File.read("data/toc.txt").split(/\n/)
   erb :home
 end
+
+get "/chapter/1" do
+  @title = "Chapter 1"
+  @chapter = File.readlines("data/chp1.txt")
+  @contents = File.readlines("data/toc.txt")
+
+  erb :chapter
+end
