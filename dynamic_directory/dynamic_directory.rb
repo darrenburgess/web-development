c777577c2ccd0930821b378953c5811fbbf4aaf9
Darren Burgess <darrentburgess@gmail.com>
2016-06-25 18:10:03 -0400

    complete views and logic for displaying files

--- dynamic_directory/dynamic_directory.rb
+++ dynamic_directory/dynamic_directory.rb
@@ -1,9 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "pry"
 
 get "/" do
   @title = "File List"
-  @files = Dir.glob('public/*.txt').map { |file| file[7..-1]}
+  @files = Dir.glob('public/*.txt').map { |file| file[7..-1] }
+  @sort = @params["sort"]
+
+  if @sort == "descending"
+    @files.reverse!
+  else
+    @files.sort!
+  end
+
   erb :home
 end
