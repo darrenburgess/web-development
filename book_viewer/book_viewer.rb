93d4b329c835f2f9e2124612013618202129c67f
Darren Burgess <darrentburgess@gmail.com>
2016-06-26 13:56:23 -0400

    add route parameters for chapters

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "pry"
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
@@ -8,10 +9,15 @@ get "/" do
   erb :home
 end
 
-get "/chapter/1" do
-  @title = "Chapter 1"
-  @chapter = File.readlines("data/chp1.txt")
+get "/chapters/:number" do
+  number = params['number'].to_i
+  @chapter = File.readlines("data/chp#{number}.txt")
   @contents = File.readlines("data/toc.txt")
-
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number} | #{ chapter_name }"
   erb :chapter
+end
+
+get "/show/:name" do
+  params[:name]
 end
