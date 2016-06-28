d0265baa9e36d5ab1f77003004c8d8294348c837
Darren Burgess <darrentburgess@gmail.com>
2016-06-28 09:44:47 -0400

    add layout and helpers for counting users and interests

--- user_interests/user_interests.rb
+++ user_interests/user_interests.rb
@@ -6,6 +6,17 @@ require 'pry'
 
 before do
   @users = YAML.load_file "users.yaml"
+  @user_count = @users.size
+end
+
+helpers do
+  def count_interests
+    interest_count = 0
+    @users.each do |_ , hash|
+      interest_count = interest_count + hash[:interests].size
+    end
+    interest_count
+  end
 end
 
 not_found do
@@ -23,5 +34,8 @@ end
 get "/user/:name" do
   @name = params["name"].to_sym
   @user = @users.select { |key, _| key == @name }
+  redirect "/" if @user.empty?
+  @interests = @user[@name][:interests].join ", "
+  @other_users = @users.select { |key, _| key != @name }
   erb :user
 end
